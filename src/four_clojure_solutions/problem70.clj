f3dbcf5637c6b1c9929a13fe161efb076dcb2793
Yasuyuki Abe <yasu@yasuabe.net>
2020-02-16 00:04:15 +0900

    solve 70

---
+++ src/four_clojure_solutions/problem70.clj
@@ -0,0 +1,22 @@
+(ns four-clojure-solutions.problem70
+  (:require [four-clojure-solutions.checker :refer [checker]]))
+
+(checker
+ (fn [s]
+   (->> (clojure.string/split s #"[ .!?\\-]")
+        (map #(list (.toUpperCase %1) %1))
+        (sort-by first)
+        (map second)))
+
+ #(= (%  "Have a nice day.")
+     ["a" "day" "Have" "nice"])
+
+ #(= (%  "Clojure is a fun language!")
+     ["a" "Clojure" "fun" "is" "language"])
+
+ #(= (%  "Fools fall for foolish follies.")
+     ["fall" "follies" "foolish" "Fools" "for"]))
+
+
+
+
