c76135f4b1f1497397606be93c5b857bbd4953b3
Yasuyuki Abe <yasu@yasuabe.net>
2020-02-24 01:49:40 +0900

    solve 98

--- src/four_clojure_solutions/problem98.clj
+++ src/four_clojure_solutions/problem98.clj
@@ -1,19 +1,23 @@
-(ns four-clojure-solutions.problem65
+(ns four-clojure-solutions.problem98
   (:require [four-clojure-solutions.checker :refer [checker]]))
 
 (checker
- (fn [seq]
-   (case (conj (empty seq) [0 0] [0 1])
-     {0 1} :map
-     #{[0 0] [0 1]} :set
-     [[0 0] [0 1]] :vector
-     '([0 1] [0 0]) :list))
- 
- #(= :map (% {:a 1, :b 2}))
- #(= :list (% (range (rand-int 20))))
-
- #(= :vector (% [1 2 3 4 5 6]))
- #(= :set (% #{10 (rand-int 5)}))
-
- #(= [:map :set :vector :list] (map % [{} #{} [] ()])))
-
+ (fn [f s]
+   (into
+    #{}
+    (vals
+     (reduce
+      (fn [acc e]
+        (let [k (f e)
+              cur (acc k)]
+          (assoc acc k (if (nil? cur) #{e} (conj cur e)))))
+      {}
+      s))))
+ (fn [f] (= (f #(* % %) #{-2 -1 0 1 2})
+            #{#{0} #{1 -1} #{2 -2}}))
+ (fn [f] (= (f #(rem % 3) #{0 1 2 3 4 5 })
+            #{#{0 3} #{1 4} #{2 5}}))
+ (fn [f] (= (f identity #{0 1 2 3 4})
+            #{#{0} #{1} #{2} #{3} #{4}}))
+ (fn [f] (= (f (constantly true) #{0 1 2 3 4})
+            #{#{0 1 2 3 4}})))
